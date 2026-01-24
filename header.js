a3a30c0c5e2ade80389f5af05d1dad44e42170e8
dipa-cotton <diyabunny13@gmail.com>
2026-01-24 11:30:34 -0500

    major changes, added a photo and an event along w some css

--- header.js
+++ header.js
@@ -5,10 +5,11 @@ class SiteHeader extends HTMLElement {
         <nav class="navbar">
         <div class="menu-toggle" id="menu-toggle">☰</div>
         <ul class="nav-links" id="nav-links">
+        <a href="home.html"><img src="images/smal.png" alt="Jobees Robotics Logo" class="logo"></a>
+        <h2 class="site-title" style="bold;">Jobees Robotics </h2> 
           <li><a href="home.html">Home</a></li>
-          <li><a href="first.html">FIRST Robotics</a></li>
+          <li><a href="first.html">FIRST</a></li>
           <li><a href="events.html">Events</a></li>
-          <li><a href="sponsors.html">Sponsors</a></li>
           <li><a href="about.html">About</a></li>
           <li><a href="resources.html">Resources</a></li>
           <li><a href="reference.html">Reference Page</a></li>
