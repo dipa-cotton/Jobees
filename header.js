b30edb5414d10bedd3de6adf4b7204cb86a4f36d
dipa-cotton <diyabunny13@gmail.com>
2026-01-24 12:01:50 -0500

    new event!

--- header.js
+++ header.js
@@ -2,19 +2,28 @@ class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="header">
-        <nav class="navbar">
-        <div class="menu-toggle" id="menu-toggle">☰</div>
-        <ul class="nav-links" id="nav-links">
-        <a href="home.html"><img src="images/smal.png" alt="Jobees Robotics Logo" class="logo"></a>
-        <h2 class="site-title" style="bold;">Jobees Robotics </h2> 
-          <li><a href="home.html">Home</a></li>
-          <li><a href="first.html">FIRST</a></li>
-          <li><a href="events.html">Events</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="reference.html">Reference Page</a></li>
-        </ul>
-      </nav>
-    </header>
+  <nav class="navbar">
+
+    <div class="menu-toggle" id="menu-toggle">☰</div>
+
+    <div class="nav-left">
+      <a href="home.html">
+        <img src="images/smal.png" alt="Jobees Robotics Logo" class="logo">
+      </a>
+      <h2 class="site-title">Jobees Robotics</h2>
+    </div>
+
+    <ul class="nav-links" id="nav-links">
+      <li><a href="home.html">Home</a></li>
+      <li><a href="first.html">FIRST</a></li>
+      <li><a href="events.html">Events</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="reference.html">Reference Page</a></li>
+    </ul>
+
+  </nav>
+</header>
+
     `;
   }
 }
