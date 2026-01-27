28e2a842cfa054bbe044d4a79b73104251dc9795
StarVeronica <137817080+StarVeronica@users.noreply.github.com>
2026-01-26 22:08:50 -0500

    Animation of cards, some style changes

--- footer.js
+++ footer.js
@@ -9,9 +9,8 @@ class SiteFooter extends HTMLElement {
         <li><a href="home.html">Home</a></li>
         <li><a href="first.html">FIRST Robotics</a></li>
         <li><a href="events.html">Events</a></li>
-        <li><a href="sponsors.html">Sponsors</a></li>
         <li><a href="about.html">About</a></li>
-        <li><a href="resources.html">Resources</a></li>
+        <li><a href="resource.html">Resources</a></li>
         <li><a href="reference.html">Reference Page</a></li>
 
       </ul>
@@ -20,8 +19,11 @@ class SiteFooter extends HTMLElement {
       <h4>Contact Us</h4>
       <p>Email: jobeesrobotics@gmail.com</p>
       <p>Phone: (123) 456-7890</p>
+      <p>© 2026 Jobees Robotics Team</p>
     </div>
-    <p>© 2026 Jobees Robotics Team</p>
+    <a href="home.html">
+        <img src="images/smal.png" alt="Jobees Robotics Logo" class="logo">
+      </a>
   </div>
 </footer>
     `;
